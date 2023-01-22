8e1bad8d14759e80f7bb695ed5af0a5bcced402c
Jamal Rothfuchs <jamal2362@googlemail.com>
2023-01-22 17:10:07 +0100

    Code cleanup

--- src/co/aospa/glyph/Services/NotificationService.java
+++ src/co/aospa/glyph/Services/NotificationService.java
@@ -16,7 +16,6 @@
 
 package co.aospa.glyph.Services;
 
-import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
@@ -32,8 +31,6 @@ import com.android.internal.util.ArrayUtils;
 import co.aospa.glyph.Constants.Constants;
 import co.aospa.glyph.Manager.AnimationManager;
 import co.aospa.glyph.Manager.SettingsManager;
-import co.aospa.glyph.Manager.StatusManager;
-import co.aospa.glyph.Utils.FileUtils;
 
 public class NotificationService extends NotificationListenerService {
 
@@ -75,7 +72,7 @@ public class NotificationService extends NotificationListenerService {
             if (packageChannel != null) {
                 packageImportance = packageChannel.getImportance();
             }
-        } catch (PackageManager.NameNotFoundException e) {};
+        } catch (PackageManager.NameNotFoundException ignored) {}
         if (DEBUG) Log.d(TAG, "onNotificationPosted: package:" + packageName + " | channel id: " + packageChannelID + " | importance: " + packageImportance);
         if (SettingsManager.isGlyphNotifsAppEnabled(this, packageName)
                         && !sbn.isOngoing()
